f00e2d63003231bd93a1ec8e70ff3f485e03dd5d
Matthew Hooper <mhooper@scottlogic.co.uk>
2019-02-05 10:33:22 +0000

    Add OlympicWinners class

---
+++ OlympicWinner.cs
@@ -0,0 +1,31 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+public class OlympicWinner {
+    public string Athlete { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public int? Age { get; set; }
+    public string Country { get; set; }
+    public int Year { get; set; }
+    [JsonProperty("date")]
+    [JsonConverter(typeof(CustomDateTimeConverter))]
+    public DateTime MedalDate { get; set; }
+    public string Sport { get; set; }
+    public int Gold { get; set; }
+    public int Silver { get; set; }
+    public int Bronze { get; set; }
+    public int Total { get; set; }
+
+    public string Details() {
+        return $"Athlete: {Athlete}. Country: {Country}. Sport: {Sport}. Year: {Year}. Age: {Age}. Gold {Gold}. Silver {Silver}. Bronze {Bronze}. Total: {Total}.";
+    }
+}
+
+public class CustomDateTimeConverter : IsoDateTimeConverter
+{
+    public CustomDateTimeConverter()
+    {
+        base.DateTimeFormat = "dd/MM/yyyy";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace filters
 {
@@ -6,7 +9,20 @@ namespace filters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<OlympicWinner> winners;
+
+            using (StreamReader file = File.OpenText(@".\data\olympicWinnersSmall.json"))
+            {
+                winners = JsonConvert.DeserializeObject<List<OlympicWinner>>(file.ReadToEnd());
+            }
+            
+            Console.WriteLine($"Winners count: {winners.Count}.");
+
+            foreach (var winner in winners.FindAll(w => w.Athlete == "Chris Hoy"))
+            {
+                Console.WriteLine(winner.Details());
+            };
+
         }
     }
 }
